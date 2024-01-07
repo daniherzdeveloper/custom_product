1c1c091600cce4e4332931581f49bca3483c7da1
admin <hernandezgarciadaniel837@gmail.com>
2024-01-06 20:58:12 -0500

    product

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Product',
+    'version': '16.0',
+    'author': 'daniherzdeveloper',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'summary': 'Product',
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/menu_view.xml',
+    ],
+}
